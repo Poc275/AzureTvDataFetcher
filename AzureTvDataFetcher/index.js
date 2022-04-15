680d0926ed487c8cbaa7c777d27193babbeff03b
Peter O'Connor <poc275@gmail.com>
2022-04-15 12:01:14 +0100

    Fetches full show info.

--- AzureTvDataFetcher/index.js
+++ AzureTvDataFetcher/index.js
@@ -8,26 +8,48 @@ module.exports = async function (context, req) {
     //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
     //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
+    const tmdbApiBaseUrl = "https://api.themoviedb.org/3";
     const type = context.bindingData.type;
     const tmdbId = context.bindingData.id;
-    
-    try {
-        const showInfoRes = await fetchShowInfo(tmdbId);
-        const responseMessage = `Getting info for a ${type} with TMDB ID ${tmdbId}: ${showInfoRes.data}`;
-    
+
+    if(type === "movie") {
         context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: responseMessage
+            status: 501,
+            body: 'Movie fetching not currently supported'
         };
-    } catch(err) {
-        context.log(`Error fetching TV show info for id ${tmdbId}: ${err}`);
-        context.res = {
-            status: err.response.status,
-            body: err.response.data
-        };
+    } else {
+        try {
+            const showInfo = await axios.get(`${tmdbApiBaseUrl}/${type}/${tmdbId}?api_key=${process.env["TmdbApiKey"]}`);
+            const appendToResponse = getTvAppendToResponse(showInfo.data);
+            const fullShowInfo = await axios.get(`${tmdbApiBaseUrl}/${type}/${tmdbId}?append_to_response=${appendToResponse}&api_key=${process.env["TmdbApiKey"]}`)
+
+            const responseMessage = `Getting info for a ${type} with TMDB ID ${tmdbId}: ${JSON.stringify(fullShowInfo.data)}`;
+        
+            context.res = {
+                // status: 200, /* Defaults to 200 */
+                body: responseMessage,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            };
+        } catch(err) {
+            context.log(`Error fetching ${type} info for id ${tmdbId}: ${err}`);
+            context.res = {
+                status: err.response.status,
+                body: err.response.data
+            };
+        }
     }
 }
 
-const fetchShowInfo = async (tmdbId) => {
-    return await axios.get(`https://api.themoviedb.org/3/tv/${tmdbId}?api_key=${process.env["TmdbApiKey"]}`);
+const getTvAppendToResponse = (showInfo) => {
+    // Creates the query params that are used in the 'append_to_response'
+    // TMDB API call to retrieve all of the required show information.
+    // For TV shows this is just the season information
+    const seasons = [];
+    showInfo.seasons.map(season => {
+        seasons.push(`season/${season.season_number}`);
+    });
+
+    return seasons.join(',');
 };
