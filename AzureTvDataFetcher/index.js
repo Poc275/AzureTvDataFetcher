7325f36ddb68e9fd2d9e10ce263ca2e23185de30
Peter O'Connor <poc275@gmail.com>
2022-04-15 11:07:26 +0100

    Adds axios to fetch API data.

--- AzureTvDataFetcher/index.js
+++ AzureTvDataFetcher/index.js
@@ -1,3 +1,5 @@
+const axios = require('axios').default;
+
 module.exports = async function (context, req) {
     // context.log('JavaScript HTTP trigger function processed a request.');
 
@@ -8,14 +10,24 @@ module.exports = async function (context, req) {
 
     const type = context.bindingData.type;
     const tmdbId = context.bindingData.id;
-    const responseMessage = `Getting info for a ${type} with TMDB ID ${tmdbId}`;
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    
+    try {
+        const showInfoRes = await fetchShowInfo(tmdbId);
+        const responseMessage = `Getting info for a ${type} with TMDB ID ${tmdbId}: ${showInfoRes.data}`;
+    
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: responseMessage
+        };
+    } catch(err) {
+        context.log(`Error fetching TV show info for id ${tmdbId}: ${err}`);
+        context.res = {
+            status: err.response.status,
+            body: err.response.data
+        };
+    }
 }
 
-const fetchShowInfo = (tmdbId) => {
-
+const fetchShowInfo = async (tmdbId) => {
+    return await axios.get(`https://api.themoviedb.org/3/tv/${tmdbId}?api_key=${process.env["TmdbApiKey"]}`);
 };
